f96a5cf3738a4e30e6c0b218b788fb08b5af33dd
RodrigoAGM <rod-gm@hotmail.com>
2021-01-20 00:05:06 -0500

    Update interfaces

--- src/interface/service/list.interface.ts
+++ src/interface/service/list.interface.ts
@@ -1,6 +1,13 @@
 import { BaseInterface } from "./base.interface";
 import { Result } from "../result.interface";
+import { Payload } from "../request.interface";
+import { List } from "../../model/list.model";
 
 export interface ListInterface extends BaseInterface {
-    
+    getFromElection(electionId: number): Promise<Result>
+    getFromElectionWithRole(userPayload: Payload, electionId: number): Promise<Result>
+    addWithRole(userPayload: Payload, electionId: number, list: List): Promise<Result>
+    getByIdWithRole(userPayload: Payload, electionId: number, listId: number): Promise<Result>
+    updateWithRole(userPayload: Payload, electionId: number, listId: number, list: List): Promise<Result>
+    deleteWithRole(userPayload: Payload, electionId: number, listId: number): Promise<Result>
 }
