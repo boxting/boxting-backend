ec6bce3d20046b33a11be14a58403e58ad16edc9
RodrigoAGM <rod-gm@hotmail.com>
2021-02-12 04:11:05 -0500

    Remove not used dependencies

--- src/service/list.service.ts
+++ src/service/list.service.ts
@@ -1,8 +1,6 @@
 // Error
-import { BadRequestError } from "../error/bad.request.error";
 import { InternalError } from "../error/base.error";
 import { NotFoundError } from "../error/not.found.error";
-import { NotPermittedError } from "../error/not.permitted.error";
 import { Payload } from "../interface/request.interface";
 // Interface
 import { Result } from '../interface/result.interface';
