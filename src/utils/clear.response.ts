4ca553bfe61a3fdfb2ad034fe3396bea9c236fc1
RodrigoAGM <rod-gm@hotmail.com>
2021-03-15 00:18:03 -0500

    Add contract to clear data validation

--- src/utils/clear.response.ts
+++ src/utils/clear.response.ts
@@ -2,7 +2,7 @@ export function clearData(objects: any) {
 
     //Clears null data and passwords
     const jsonStr = JSON.stringify(objects, (k, v) => (
-        (v === null || k === "password" || k === "roleId") ? undefined : v))
+        (v === null || k === "password" || k === "roleId" || k === "contract") ? undefined : v))
 
     return JSON.parse(jsonStr)
 }
