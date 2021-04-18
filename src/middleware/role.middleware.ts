8b1328a6d1146fb3684fd278807353e70b8f828d
RodrigoAGM <rod-gm@hotmail.com>
2021-04-18 17:55:58 -0500

    Handle error structure on autentication failure

--- src/middleware/role.middleware.ts
+++ src/middleware/role.middleware.ts
@@ -1,5 +1,5 @@
-import { Request, Response, NextFunction } from "express";
-import { TokenRequest } from "../interface/request.interface";
+import { Request, Response, NextFunction } from 'express';
+import { TokenRequest } from '../interface/request.interface';
 
 
 export function authenticateRole(roles: number[]) {
@@ -7,7 +7,7 @@ export function authenticateRole(roles: number[]) {
         const tokenRequest = req as TokenRequest
 
         if (roles.indexOf(tokenRequest.user.role) == -1) {
-            return res.status(403).send('You do not have the permission to get this data.')
+            return res.status(403).send({ 'success': false, 'error': 'You do not have the permission to get this data.' });
         }
 
         next()
